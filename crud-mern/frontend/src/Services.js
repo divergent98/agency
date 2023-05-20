5a2f9d4109e80206a7ffe36fbd7286e55d732377
divergent98 <52315589+divergent98@users.noreply.github.com>
2023-05-20 13:54:39 +0200

    Create Services.js

---
+++ crud-mern/frontend/src/Services.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import { ListGroup, ListGroupItem, Button } from "reactstrap";
+
+export const Services  = () => {
+        return (
+        <div className='container'>
+          <div className='row'>
+            <div className='col-4'>
+              
+            </div>
+            <div className='col-4'></div>
+            <div className='col-4'></div>
+          </div>
+        </div>
+           
+        );
+    
+};
+
+

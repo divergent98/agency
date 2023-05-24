871708aab47117bd0bb6423bd881189f7cfbbda7
ljiljanagospic <52315589+divergent98@users.noreply.github.com>
2023-05-24 10:16:55 +0200

    featured products edited

--- crud-mern/frontend/src/FeaturedProducts.js
+++ crud-mern/frontend/src/FeaturedProducts.js
@@ -22,8 +22,10 @@ export const FeaturedProducts  = () => {
               <><div className="row">
                 {products.map((product) => {
                   if(product.isFeatured === "true"){
-                    return(
-                    <div className="col-lg-4 col-md-12 col-sm-12" key={product._id}>
+                    return(       
+             
+                    <div className="col-lg-4 col-md-12 col-sm-12" key={product._id}>      
+                     <a href="#" >
                       <div className="card-hover">
                         <div className="card-hover__content">
                           <h3 className="card-hover__title">
@@ -32,33 +34,19 @@ export const FeaturedProducts  = () => {
                           <p class="card-hover__text">
                           {product.description}
                           </p>
-                          <a href="#" class="card-hover__link">
+                   
                             <span>See more...</span>
-                            <svg
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke-width="1.5"
-                              stroke="currentColor"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                              />
-                            </svg>
-                          </a>
+                      
+                       
                         </div>
-                        <div className="card-hover__extra">
-                          <h4>
-                          {product.image}
-                          </h4>
-                        </div>
+                 
                         <img
                           src={product.image}
                           alt=""
                         ></img>
-                      </div>
-                      </div>
+                      </div> 
+                      </a>
+                      </div>  
                    )
                   }else{
                     <h4 className="text-center">no products </h4>

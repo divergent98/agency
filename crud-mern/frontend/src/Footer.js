eb1dd3301ea9601038fa4c8420349f480bce92d1
divergent98 <52315589+divergent98@users.noreply.github.com>
2023-06-08 11:08:58 +0200

    footer

--- crud-mern/frontend/src/Footer.js
+++ crud-mern/frontend/src/Footer.js
@@ -1,36 +1,64 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import instagram from "./img/instagram.png";
 import youtube from "./img/youtube.png";
 import telegram from "./img/telegram.png";
 import whatsapp from "./img/whatsapp.png";
+import logo from "./img/logo.png";
 export const Footer = () => {
   return (
-    <div className="background-dark-gray"> 
-    <div className="container">
-      <div className="row">      
-        <div className="col-3 social">
-          <div className="logo">
-            logo
+    <div className="background-dark-gray">
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-3 social">
+            <div className="logo">
+              <img className="footer-logo" src={logo} />
+            </div>
+            <div className="social-media-icons">
+              <h6 class="gradient-headline font-18 blinker mt-4 mb-3">
+                Follow Us
+              </h6>
+              <div class="d-flex">
+                <a class="me-4" href="#">
+                  <img src={instagram} />
+                </a>
+                <a class="me-4" href="#">
+                  <img src={youtube} />
+                </a>
+                <a class="me-4" href="#">
+                  <img src={telegram} />
+                </a>
+                <a class="me-4" href="#">
+                  <img src={whatsapp} />
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="social-media-icons">
-          <h6 class="text-primary mt-4 mb-3">Follow Us</h6>
-          <div class="d-flex">
-                        <a class="mx-2" href="#"><img src={instagram} /></a>
-                        <a class="mx-2" href="#"><img src={youtube} /></a>
-                        <a class="mx-2" href="#"><img src={telegram} /></a>
-                        <a class="mx-2" href="#"><img src={whatsapp} /></a>
-                    </div>
+
+          <div className="col-3 polices">
+            <h6 class="gradient-headline font-24 blinker mt-4 mb-3">
+              Company polices
+            </h6>
+       
+          </div>
+          <div className="col-3 quick-links">
+            <h6 class="gradient-headline font-24 blinker mt-4 mb-3">
+              Site Navigation
+            </h6>
+            <ul className="font-18">
+              <li><Link className="decoration-none text-light">Home</Link></li>
+              <li><Link className="decoration-none text-light">Hot tours</Link></li>
+              <li><Link className="decoration-none text-light">Special offers</Link></li>
+              <li><Link className="decoration-none text-light">Contact</Link></li>
+            </ul>
+          </div>
+          <div className="col-3 contact">
+            <h6 class="gradient-headline font-24 blinker mt-4 mb-3">
+              Contact us
+            </h6>
           </div>
         </div>
-        <div className="col-3 contact">
-
-        </div>
-        <div className="col-3 polices"></div>
-        <div className="col-3 quick-links"></div>
-  
-
       </div>
-    </div>
     </div>
   );
 };
